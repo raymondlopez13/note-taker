4bb8c214515145409afd640b2f17c86982e31c3c
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-01 10:42:01 -0600

    added homepage

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const [ notes ] = require("./Develop/db/db.json");
+app.use(express.static('public'));
+const path = require('path');
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}`);
 });
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });

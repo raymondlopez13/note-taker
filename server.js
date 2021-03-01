4ca60c982a26732a225aef524fa41eb063925488
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-01 10:47:11 -0600

    Added route to notes page

--- server.js
+++ server.js
@@ -16,3 +16,7 @@ app.get('/api/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
